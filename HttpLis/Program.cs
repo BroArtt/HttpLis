e513d7a457190bc9763f9fdb15d265bce4635de4
Brovko Artem <artembrovko2020@gmail.com>
2022-03-31 14:49:39 +0300

    multithreadingHttplistener

--- HttpLis/Program.cs
+++ HttpLis/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HttpListenerBrovko
@@ -9,63 +10,59 @@ namespace HttpListenerBrovko
     {
         static void Main(string[] args)
         {
-            HttpListener listener = new HttpListener();
-            listener.Prefixes.Add("http://localhost:8888/");
-            listener.Start();
-            Console.WriteLine("Ожидание подключений...");
+            
+                HttpListener listener = new HttpListener();
+                listener.Prefixes.Add("http://localhost:8888/");
+                listener.Start();
+                Console.WriteLine("Ожидание подключений...");
+
+            async void Listener()
+            {
+                    HttpListenerContext context = await listener.GetContextAsync();
+                    HttpListenerRequest request = context.Request;
+                    HttpListenerResponse response = context.Response;
+
+                        var staticFilesDirectory = "C:/Users/Artem/source/repos/App1/HttpListest/HttpLis/HttpLis/HTMLPage/";
+                        var file = string.Empty;
+                 
+                        if (request.Url.AbsolutePath == "/")
+                        {
+                            file = "Brovko.html";
+                        }
+                        else
+                        {
+                            file = request.Url.AbsolutePath.Trim('/');
+                        }
+
+                        var staticFileToUpload = Path.Combine(staticFilesDirectory, file);
+
+                        var ErrorPage = Path.Combine(staticFilesDirectory, "ErrorPage.html");
+
+                        var contentToUpload = string.Empty;
+
+                        if (File.Exists(staticFileToUpload) && staticFileToUpload.StartsWith(staticFilesDirectory))
+                        {
+                            contentToUpload = File.ReadAllText(staticFileToUpload);
+                        }
+                        else
+                        {
+                            response.StatusCode = (int)HttpStatusCode.NotFound;
+                            contentToUpload = File.ReadAllText(ErrorPage);
+                        }
 
 
+                        byte[] bufferhtml = System.Text.Encoding.UTF8.GetBytes(contentToUpload);
+
+                        response.ContentLength64 = bufferhtml.Length;
+                        Stream output = response.OutputStream;
+                        output.Write(bufferhtml, 0, bufferhtml.Length);
+                        output.Close();
+                
+        }
             while (true)
             {
-                HttpListenerContext context = listener.GetContext();
-
-                HttpListenerRequest request = context.Request;
-                HttpListenerResponse response = context.Response;
-
-
-
-              //Console.WriteLine(request.Url);
-              // Console.WriteLine(request.Url.PathAndQuery);
-
-                var staticFilesDirectory = "C:/Users/Hp/source/repos/HttpLis/HttpLis/HTMLPage/";
-                var file = string.Empty;
-
-
-                if (request.Url.AbsolutePath == "/")
-                {
-                    file = "Brovko.html";
-                }
-                else
-                {
-                    file = request.Url.AbsolutePath.Trim('/');
-                }
-
-                var staticFileToUpload = Path.Combine(staticFilesDirectory, file);
-
-                var ErrorPage = Path.Combine(staticFilesDirectory, "ErrorPage.html");
-
-                var contentToUpload = string.Empty;
-
-                if (File.Exists(staticFileToUpload) && staticFileToUpload.StartsWith(staticFilesDirectory))
-                {
-                    contentToUpload = File.ReadAllText(staticFileToUpload);
-                }
-                else
-                {
-                    response.StatusCode = (int)HttpStatusCode.NotFound;
-                    contentToUpload = File.ReadAllText(ErrorPage);
-                }
-
-
-                byte[] bufferhtml = System.Text.Encoding.UTF8.GetBytes(contentToUpload);
-
-                response.ContentLength64 = bufferhtml.Length;
-                Stream output = response.OutputStream;
-                output.Write(bufferhtml, 0, bufferhtml.Length);
-                output.Close();
-
+              Listener();
             }
-
-        }
+           }
     }
 }
